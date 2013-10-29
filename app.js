78973519fde95623882164fc97bfe3bc44f6496e
Arvind Palaniswamy <parvind_15@hotmail.com>
2013-10-29 11:03:55 -0700

    authenticate google

--- app.js
+++ app.js
@@ -129,5 +129,39 @@
     });*/
 
     // On initial load, start by fetching the current data
-    refreshTodoItems();
+    //refreshTodoItems();
+function refreshAuthDisplay() {
+    var isLoggedIn = client.currentUser !== null;
+    $("#logged-in").toggle(isLoggedIn);
+    $("#logged-out").toggle(!isLoggedIn);
+
+
+	if (isLoggedIn) {
+	    $("#login-name").text(client.currentUser.userId);
+	    refreshTodoItems();
+	}
+}
+
+
+function logIn() {
+    client.login("google").then(refreshAuthDisplay, function(error){
+        alert(error);
+    });
+}
+
+
+function logOut() {
+    client.logout();
+    refreshAuthDisplay();
+    $('#summary').html('<strong>You must login to access data.</strong>');
+}
+
+
+// On page init, fetch the data and set up event handlers
+$(function () {
+    refreshAuthDisplay();
+    $('#summary').html('<strong>You must login to access data.</strong>');          
+    $("#logged-out button").click(logIn);
+    $("#logged-in button").click(logOut);
+});
 });
